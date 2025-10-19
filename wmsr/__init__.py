e40e960185c26711d50b9b6703c5805f517fc727
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-10-19 17:41:19 -0500

    Se crearon y se registraron las vistas para para categorias y documento recibo

--- wmsr/__init__.py
+++ wmsr/__init__.py
@@ -7,11 +7,13 @@ def create_app():
     app.config.from_object('config.Config')
 
     #Registrar vistas (blueprints)
-    from wmsr import home, auth, productos, ubicaciones
+    from wmsr import home, auth, productos, ubicaciones, categorias, document_recibo
     app.register_blueprint(home.bp) #vista de home
     app.register_blueprint(auth.bp) #vista de auth
     app.register_blueprint(productos.bp) #vista de productos
     app.register_blueprint(ubicaciones.bp) #vista de ubicaciones
+    app.register_blueprint(categorias.bp) #vista de categorias
+    app.register_blueprint(document_recibo.bp) #vista de document_recibo
 
 
     
