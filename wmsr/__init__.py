8b9bab7d2839d8660695ca3aff00264183e5dd33
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-10-19 17:12:23 -0500

    Registro de la primera vista (pagina de bienvenida) y prueba con la plantilla

--- wmsr/__init__.py
+++ wmsr/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 def create_app():
     #crear la app
-    app = Flask
-    (__name__)
+    app = Flask(__name__)
     #llamamos la configuracion de config.py
     app.config.from_object('config.Config')
+
+    #Registrar vistas (blueprints)
+    from wmsr import home
+    app.register_blueprint(home.bp) #vista de home
 
     
 
