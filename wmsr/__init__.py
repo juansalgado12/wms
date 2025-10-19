60baf0efea57663fb3a7340b71faf692faced27c
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-10-19 10:19:10 -0500

    Primera ejecución de la app, se creó una ruta principal, se creó una configuración basica y se modificó el html para llamarse index

--- wmsr/__init__.py
+++ wmsr/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    #crear la app
+    app = Flask
+    (__name__)
+    #llamamos la configuracion de config.py
+    app.config.from_object('config.Config')
+
+    
 
     return app
