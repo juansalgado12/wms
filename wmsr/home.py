a5227eb6617fdc0b3aa0d7b23da56d6180c5b173
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-10-20 15:07:57 -0500

    Se creó un archivo base.html que servirá como pagina principal del almacén

--- wmsr/home.py
+++ wmsr/home.py
@@ -5,3 +5,7 @@ bp = Blueprint('home', __name__)
 @bp.route('/')
 def welcome():
     return render_template('welcome.html')
+
+@bp.route('/almacen')
+def almacen():
+    return render_template('layouts/base.html')
