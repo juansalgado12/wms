6874e76057bf68b2e9dd989637a014c8a9f1b2e9
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-10-21 09:57:09 -0500

    Se añadieron comentarios en las rutas de home, productos y categorias

--- wmsr/categorias.py
+++ wmsr/categorias.py
@@ -2,14 +2,17 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('categorias', __name__, url_prefix='/categorias')
 
+# Ruta para la lista de categorías
 @bp.route('/')
 def lista_categorias():
     return render_template('categorias/listacategorias.html')
 
+# Ruta para crear una nueva categoría
 @bp.route('/crear')
 def crear_categoria():
     return render_template('categorias/crearcategorias.html')
 
+# Ruta para editar una categoría existente
 @bp.route('/editar')
 def editar_categoria():
     return render_template('categorias/editarcategorias.html')
--- wmsr/productos.py
+++ wmsr/productos.py
@@ -2,14 +2,17 @@ from flask import blueprints, render_template
 
 bp = blueprints.Blueprint('productos', __name__, url_prefix='/productos')
 
+# Ruta para el catálogo de productos
 @bp.route('/')
 def catalogo():
     return render_template('productos/catalogo.html')
 
+# Ruta para crear un nuevo producto
 @bp.route('/crear')
 def crear():
     return render_template('productos/crear.html')
 
+# Ruta para editar un producto existente
 @bp.route('/editar')
 def editar():
     return render_template('productos/editar.html')
