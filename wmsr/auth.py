659091268c4a2e277204b8f2283e582239a477b1
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-10-25 16:23:59 -0500

    Se importaron las funciones necesarias para la gestion de los usuarios y se dejó comentada la linea para importar el modelo de usuarios y tambien la linea para importar la base de datos

--- wmsr/auth.py
+++ wmsr/auth.py
@@ -1,4 +1,19 @@
-from flask import blueprints, render_template
+from flask import blueprints, render_template, request, session, url_for, redirect, redirect, flash, g
+#blueprint para las rutas
+#render_template para renderizar las plantillas HTML
+#request para manejar las solicitudes HTTP
+#session para manejar la sesión del usuario
+#url_for para generar URLs para las rutas
+#redirect para redirigir a otras rutas
+#flash para mostrar mensajes flash
+#g para almacenar datos durante la solicitud (o guardar datos relacionados con la sesion de usuario)
+
+from werkzeug.security import generate_password_hash, check_password_hash
+#generate_password_hash para hashear contraseñas
+#check_password_hash para verificar contraseñas hasheadas
+
+#from .models import User (cuando esta disponible el modelo User)
+#from wmsr import db (cuando esta disponible la instancia de la base de datos)
 
 bp = blueprints.Blueprint('auth', __name__, url_prefix='/auth')
 
