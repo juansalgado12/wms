5aaad2f142144cffb2be6e45034aed6a3f9b51a7
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-10-29 16:30:29 -0500

    Se descomentó la funcion de registro de usuarios y se adaptaron los nombres de los modelos y atributos

--- wmsr/auth.py
+++ wmsr/auth.py
@@ -12,33 +12,33 @@ from werkzeug.security import generate_password_hash, check_password_hash
 #generate_password_hash para hashear contraseñas
 #check_password_hash para verificar contraseñas hasheadas
 
-#from .models import User (cuando esta disponible el modelo User)
-#from wmsr import db (cuando esta disponible la instancia de la base de datos)
+from .models import Usuarios
+from wmsr import db 
 
 bp = blueprints.Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/registro', methods=('GET', 'POST'))
 def registro():
-    # if request.method == 'POST': #si el metodo es POST
-    #     username = request.form.get('username') #obtener el nombre de usuario del formulario
-    #     email = request.form.get('email')#obtener el email del formulario
-    #     password = request.form.get('password')#obtener la contraseña del formulario
+    if request.method == 'POST': #si el metodo es POST
+        username = request.form.get('username') #obtener el nombre de usuario del formulario
+        email = request.form.get('email')#obtener el email del formulario
+        password = request.form.get('password')#obtener la contraseña del formulario
 
-    #     user = User(username, email, generate_password_hash(password)) #crear una instancia del usuario con la contraseña hasheada
+        user = Usuarios(usu_nombre=username, usu_email=email, usu_password=generate_password_hash(password)) #crear una instancia del usuario con la contraseña hasheada
 
-    #     # validar los datos del formulario
-    #     error = None
-    #     user_email = User.query.filter_by(email = email).first() #verificar si el email ya existe en la base de datos
+        # validar los datos del formulario
+        error = None
+        user_email = Usuarios.query.filter_by(usu_email=email).first() #verificar si el email ya existe en la base de datos
 
-    #     if user_email == None:
-    #         #si no existe el email, agregar el usuario a la base de datos
-    #         db.session.add(user)
-    #         db.session.commit()
-    #         return redirect(url_for('auth.login'))
-    #     else:
-    #         #de lo contrario, mostrar un mensaje de error
-    #         error = f'El correo {email}' 'ya está registrado.'
-    #     flash(error)
+        if user_email == None:
+            #si no existe el email, agregar el usuario a la base de datos
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+        else:
+            #de lo contrario, mostrar un mensaje de error
+            error = f'El correo {email}' 'ya está registrado.'
+        flash(error)
     return render_template('auth/registro.html')
 
 @bp.route('/login', methods = ('GET', 'POST'))
