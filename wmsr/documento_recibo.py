a14fefbeece6d0be5957cbd545cae5b422320e65
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-11-10 10:36:47 -0500

    Añadida funcionalidad para listar documentos de recibo, incluyendo la obtención de proveedores y un mapeo de IDs a nombres para facilitar el acceso en la plantilla.

--- wmsr/documento_recibo.py
+++ wmsr/documento_recibo.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+from .auth import login_required
+from .models import DocumentoRecibo, Proveedor
+from . import db
 
 bp = Blueprint('documento_recibo', __name__, url_prefix='/documento_recibo')
 
 @bp.route('/')
 def lista_documentos():
-    return render_template('documento_recibo/listadocumentos.html')
+    # obtener todos los documentos de recibo de la base de datos y proveedores relacionados
+
+    documentos = DocumentoRecibo.query.all()
+    proveedores = Proveedor.query.all()
+
+    # Mapear los IDs de proveedores a sus nombres para un acceso rápido
+    proveedores_map = {p.prov_id: p.prov_nombre for p in proveedores}
+
+
+    return render_template('documento_recibo/listadocumentos.html', documentos=documentos, proveedores=proveedores, proveedores_map=proveedores_map)
 
 @bp.route('/crear')
 def crear_documento():
