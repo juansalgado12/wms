dbe2daa3aab560dc0738307322a835d4c789dfa1
juansalgado12 <juan.salgado02@uceva.edu.co>
2025-11-11 15:21:26 -0500

    Añadida la funcionalidad de gestión de ubicaciones: se crearon las vistas para listar, crear y editar ubicaciones en el inventario (vacias de momento).

--- wmsr/ubicaciones.py
+++ wmsr/ubicaciones.py
@@ -1,15 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from wmsr.utils.export_excel import exportar_a_excel
+
+from .auth import login_required # Importar el decorador de login requerido si es necesario
+from .models import Ubicaciones, Categorias # Importar el modelo de Inventario y Ubicaciones
+from . import db # Importar la base de datos
 
 bp = Blueprint('ubicaciones', __name__, url_prefix='/ubicaciones')
 
 @bp.route('/')
 def lista_ubicaciones():
-    return 'pagina de lista de ubicaciones'
+    return render_template('inventario/ubicaciones/listaubicaciones.html')
 
 @bp.route('/crear')
 def crear_ubicacion():
-    return 'Pagina de crear ubicacion'
+    return render_template('inventario/ubicaciones/crearubicaciones.html')
 
 @bp.route('/editar')
 def editar_ubicacion():
-    return 'Pagina de editar ubicacion'
+    return render_template('inventario/ubicaciones/editarubicaciones.html')
